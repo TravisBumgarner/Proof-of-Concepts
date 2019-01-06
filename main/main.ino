17c8456318e4dfc025de2695b8daf21ed242ea26
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-01-06 18:59:39 -0500

    Push button writes to file

--- main/main.ino
+++ main/main.ino
@@ -4,46 +4,70 @@
 Sd2Card card;
 SdVolume volume;
 SdFile root;
-File image;
+File file;
 
+const int BUTTON_UP_PIN = 15;
+const int BUTTON_DOWN_PIN = 0;
+const int SD_CS_PIN = 16;
 
-const int SD_CHIP_SELECT = 16;
+int upButtonState = 0;
+int previousUpButtonState = 0;
+int downButtonState = 0;
+
+void print(String text){
+  Serial.print(text);
+}
+
+void println(String text){
+  Serial.println(text);
+}
+
+void writeToSDCard(String fileName, String contents){
+  file = SD.open(fileName, FILE_WRITE);
+  if (file) {
+    println("Writing to " + fileName);
+    file.println(contents);
+    file.close();
+    println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    println("error opening test.txt");
+  }
+}
 
 bool setupSDCard(){
   Serial.print("Initializing SD Card...");
   
-  if(SD.begin(SD_CHIP_SELECT)){
-    Serial.print("initialization success!\n");
+  if(SD.begin(SD_CS_PIN)){
+    println("initialization success!");
     return true;
   } else {
-    Serial.print("initialization failed!\n");
+    println("initialization failed!");
     return false;
   }
 }
 
-
+void setupButtons(){ 
+  pinMode(BUTTON_UP_PIN, INPUT);
+  pinMode(BUTTON_DOWN_PIN, INPUT); 
+}
 
 void setup() {
   Serial.begin(9600);
   bool wasSDSetup = setupSDCard();
+  setupButtons();
   
   if(!(wasSDSetup)){
     return;  
   }
-  image = SD.open("test.txt", FILE_WRITE);
-  if (image) {
-    Serial.print("Writing to test.txt...");
-    image.println("testing 1, 2, 3.");
-    // close the file:
-    image.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  upButtonState = digitalRead(BUTTON_UP_PIN);
+  if (upButtonState == HIGH && previousUpButtonState == LOW){
+    println("Saving....");
+    writeToSDCard("hello.txt", "Hello!");
+  } 
+  previousUpButtonState = upButtonState;
 }
