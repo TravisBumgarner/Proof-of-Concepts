7d5cf31daddfb5ad9605632682d57f83d58af248
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-11-28 17:15:06 -0500

    Can get a paddle in 3d Space to convert to a value of 0 to 100

--- main.py
+++ main.py
@@ -1,38 +1,91 @@
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
 import numpy as np
+import argparse
 import cv2
-import pygame
-from pygame.locals import *
+import imutils
+import time
 
-WIDTH = 320
-HEIGHT = 240
+WEBCAM_HEIGHT = 400
+paddle_location_as_percentage = 0
 
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-pygame.init()
-pygame.display.set_caption("OpenCV camera stream on Pygame")
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+# Gotten from color_detection script.
+greenLower = (90, 180, 200)
+greenUpper = (110, 255, 255)
+pts = deque(maxlen=10)
 
+vs = VideoStream(src=0).start()
+
+# allow the camera or video file to warm up
+time.sleep(2.0)
+
+# keep looping
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    # grab the current frame
+    frame = vs.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # handle the frame from VideoCapture or VideoStream
+    # frame = frame[1] if args.get("video", False) else frame
 
-    # img = cv2.line(gray, (0, 0), (511, 511), (255, 0, 0), 5)
-    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    # Display the resulting frame
-    cv2.imshow("frame", thresh)
-
-    screen.fill([125, 0, 0])
-    # screen.blit(frame, (0, 0))
-    pygame.display.update()
-
-    # Draw a diagonal blue line with thickness of 5 px
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    # if we are viewing a video and we did not grab a frame,
+    # then we have reached the end of the video
+    if frame is None:
         break
 
-# When everything done, release the capture
-cap.release()
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, height=WEBCAM_HEIGHT)
+
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    cv2.imshow("Mask", mask)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        paddle_location_as_percentage = (y / WEBCAM_HEIGHT) * 100
+        print(paddle_location_as_percentage)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+    # update the points queue
+    # This code below looks  weird. Deleted the loop.
+    pts = [center]
+
+    if pts[0] is not None:
+        thickness = 10
+        cv2.line(frame, pts[0], pts[0], (0, 0, 255), thickness)
+
+    # show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+
+    # vs.stop()
+
+# close all windows
 cv2.destroyAllWindows()
