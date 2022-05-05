8b2c49702a00f87fbc58f566ec2e5c1c1d982421
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-05 11:59:50 +0200

    Added basic canvas

--- paint-app/frontend/src/App/App.tsx
+++ paint-app/frontend/src/App/App.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
 import { Body, Title } from 'sharedComponents'
 
+const FakePixel = styled.button`
+  border: 0;
+  background-color: ${({ color }) => color};
+  width: 50px;
+  height: 50px;
+`
+
+const COLORS = ['red', 'green', 'blue', 'yellow']
+
+const getRandomColor = (colors: string[]) => colors[Math.floor(Math.random() * colors.length)];
+
 const App = () => {
+  const [colors, setColors] = React.useState<string[]>(COLORS)
   return (
     <Body>
-      <Title>Hello World!</Title>
+      <Title>Canvas</Title>
+      <div>
+        {colors.map((color, index) => (
+          <FakePixel
+            color={color}
+            key={index}
+            onClick={() => setColors(prev => {
+              const newColors = [...prev]
+              newColors[index] = getRandomColor(COLORS)
+              return newColors
+            })}
+          />)
+        )}
+      </div>
     </Body>
   )
 }
