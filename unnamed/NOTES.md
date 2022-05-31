29a66d009d035ff125e2562952760090527d3703
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-31 14:47:30 +0200

    Can connect to get data

---
+++ unnamed/NOTES.md
@@ -0,0 +1,3 @@
+# Learning Resources
+
+https://consensys.net/blog/developers/how-to-fetch-and-update-data-from-ethereum-with-react-and-swr/
--- unnamed/frontend/src/App/App.tsx
+++ unnamed/frontend/src/App/App.tsx
@@ -1,12 +1,70 @@
 import * as React from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import useSWR from 'swr'
+import { formatEther } from '@ethersproject/units'
 
 import { Body, Title } from 'sharedComponents'
 
+export const injectedConnector = new InjectedConnector({
+  supportedChainIds: [
+    4, // Rinkeby
+  ],
+})
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+const fetcher = (library: any) => (...args: any) => {
+  const [method, ...params] = args
+  console.log(method, params)
+  return library[method](...params)
+}
+
+export const Balance = () => {
+  const { account, library } = useWeb3React<Web3Provider>()
+  const { data: balance } = useSWR(['getBalance', account, 'latest'], {
+    fetcher: fetcher(library),
+  })
+  if (!balance) {
+    return <div>...</div>
+  }
+  return <div>Ξ {parseFloat(formatEther(balance)).toPrecision(4)}</div>
+}
+
+const Wallet = () => {
+  const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
+
+  const onClick = () => {
+    activate(injectedConnector)
+  }
+
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      {active ? (
+        <div>✅ </div>
+      ) : (
+        <button type="button" onClick={onClick}>
+          Connect
+        </button>
+      )}
+    </div>
+  )
+}
+
 const App = () => {
   return (
-    <Body>
-      <Title>Hello World!</Title>
-    </Body>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Wallet />
+      <Balance />
+    </Web3ReactProvider>
   )
 }
 
