65abe31409783256b59e3ae2dadb9316fd889a3b
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-31 15:02:17 +0200

    Added way to refresh balance

--- unnamed/frontend/src/App/App.tsx
+++ unnamed/frontend/src/App/App.tsx
@@ -28,21 +28,33 @@ const fetcher = (library: any) => (...args: any) => {
 
 export const Balance = () => {
   const { account, library } = useWeb3React<Web3Provider>()
-  const { data: balance } = useSWR(['getBalance', account, 'latest'], {
+  const { data: balance, mutate } = useSWR(['getBalance', account, 'latest'], {
     fetcher: fetcher(library),
   })
+
+  React.useEffect(() => {
+    // listen for changes on an Ethereum address
+    console.log(`listening for blocks...`)
+    library.on('block', () => {
+      console.log('update balance...')
+      mutate(undefined, true)
+    })
+    // remove listener when the component is unmounted
+    return () => {
+      library.removeAllListeners('block')
+    }
+    // trigger the effect only on component mount
+  }, [])
+
   if (!balance) {
     return <div>...</div>
   }
   return <div>Ξ {parseFloat(formatEther(balance)).toPrecision(4)}</div>
 }
 
+
 const Wallet = () => {
   const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
-
-  const onClick = () => {
-    activate(injectedConnector)
-  }
 
   return (
     <div>
