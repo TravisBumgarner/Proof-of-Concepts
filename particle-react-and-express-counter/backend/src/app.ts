5f96b15be90132f70e412c3e50180808368e3136
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-04-25 20:05:47 -0600

    added mongo

--- particle-react-and-express-counter/backend/src/app.ts
+++ particle-react-and-express-counter/backend/src/app.ts
@@ -2,7 +2,14 @@ require('dotenv').config()
 import express from 'express';
 import cors from 'cors'
 
-import * as db from './db'
+import { MongoClient } from "mongodb";
+
+const getClient = async () => {
+  // This bad, I'm lazy.
+  const client = new MongoClient('mongodb://root:password@localhost:27123/?authMechanism=DEFAULT');
+  const conn = await client.connect();
+  return conn.db("counter");
+}
 
 const app = express()
 
@@ -18,7 +25,17 @@ app.use((req, res, next) => {
 app.use(cors({ origin: ['localhost:3000',] }))
 
 app.get('/', (req: express.Request, res: express.Response) => {
+
   res.send('pong!')
+})
+
+
+app.get('/webhook', async (req: express.Request, res: express.Response) => {
+  const db = await getClient()
+  let collection = await db.collection("counter");
+  collection.insertOne({ date: new Date(), counter: Math.random() })
+
+  res.send('success')
 })
 
 export default app
---
+++ particle-react-and-express-counter/readme.md
@@ -0,0 +1,1 @@
+`mongosh admin -u root -p password --port 27123`
