98c88cae01b1545fb7007f789b8ef86ea994427a
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-04-25 19:46:23 -0600

    init

---
+++ particle-react-and-express-counter/counter/src/counter.cpp
@@ -0,0 +1,48 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/travisbumgarner/Programming/proof-of-concepts/particle-react-and-express-counter/counter/src/counter.ino"
+void setup();
+void loop();
+#line 1 "/Users/travisbumgarner/Programming/proof-of-concepts/particle-react-and-express-counter/counter/src/counter.ino"
+SYSTEM_THREAD(ENABLED);      // Code Runs even with no connection
+SerialLogHandler logHandler; // This allows for USB serial debug logs
+
+int count();
+
+const char *eventName = "countEvent";
+std::chrono::milliseconds publishPeriod = 30s; // Can use `min` and `h` as well.
+
+unsigned long lastPublishMs;
+
+void setup()
+{
+}
+
+void loop()
+{
+    if (millis() - lastPublishMs >= publishPeriod.count())
+    {
+        lastPublishMs = millis();
+
+        String eventData = String::format("%d", count());
+
+        if (Particle.connected())
+        {
+            Particle.publish(eventName, eventData);
+            Log.info("published %s", eventData.c_str());
+        }
+        else
+        {
+            Log.info("not cloud connected %s", eventData.c_str());
+        }
+    }
+}
+
+int counter = 0;
+int count()
+{
+    return counter++;
+}
