5900531d1b6149d58de6f7cfd5957050be7f58fc
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-04-25 20:56:14 -0600

    rest of the elephant

--- particle-react-and-express-counter/frontend/src/App.tsx
+++ particle-react-and-express-counter/frontend/src/App.tsx
@@ -1,13 +1,36 @@
-import { PageHeader, Alert, Router, Navigation } from "./components"
+import axios from 'axios'
+import { useCallback, useEffect, useState } from 'react'
+
+type CounterEvent = {
+  counter: number // '9',
+  publishedAt: Date // '2023-04-26T02:24:00.652Z',
+  _id: string // 'e00fce687cc15f2e42da5d0a'
+}
 
 const App = () => {
+  const [data, setData] = useState<CounterEvent[]>([])
+
+  const pollData = useCallback(async () => {
+    const response = await axios.get('http://localhost:5001/counter')
+    // Bad code, why yes.
+    setData((response.data as CounterEvent[]).map(({ publishedAt, ...rest }) => ({
+      publishedAt: new Date(publishedAt),
+      ...rest
+    })))
+  }, [])
+
+  useEffect(() => {
+    setInterval(pollData, 1000)
+  }, [])
+
+  console.log(data)
   return (
-    <div>
-      <PageHeader />
-      <Navigation />
-      <Alert />
-      <Router />
-    </div>
+    <ul>
+      {data
+        .sort((a, b) => a.publishedAt.getTime() - b.publishedAt.getTime())
+        .map(({ counter, publishedAt, _id }) => (<li key={_id}>{publishedAt.toLocaleDateString()} - {counter}</li>)
+        )}
+    </ul>
   )
 }
 
