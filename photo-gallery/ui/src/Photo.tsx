190e4482648c34eca0282cd29cdc15cd44f4db4d
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-26 14:23:59 -0600

    works without animation

--- photo-gallery/ui/src/Photo.tsx
+++ photo-gallery/ui/src/Photo.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { motion, useAnimationControls, useInView } from 'framer-motion'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
 import { type PhotoType } from 'types'
 import { PHOTO_SPACING } from './consts'
 import { getPhotoUrl } from './utils'
+
+const Cell = ({
+  vibrantcolors: { darkVibrant, lightVibrant },
+  src
+}: PhotoType) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const controls = useAnimationControls()
+  const isInView = useInView(ref, { amount: 0.8 })
+
+  // const wasInView = useRef(false)
+
+  // useEffect(() => {
+  //   if (isInView && !wasInView.current) {
+  //     void controls.start('fadeIn')
+  //   } else if (!isInView && wasInView.current) {
+  //     void controls.start('fadeOut')
+  //   }
+  // }, [controls, isInView])
+
+  return (
+    <StyledCell
+      ref={ref}
+      $opacity={isInView ? 1 : 0}
+      $hoverBorderColor={lightVibrant!}
+      $borderColor={darkVibrant!}
+      animate={controls}
+      transition={{
+        duration: 4
+      }}
+      variants={{
+        fadeIn: {
+          opacity: [0, 1]
+        },
+        fadeOut: {
+          rotate: [1, 0]
+        }
+      }}
+    >
+      <StyledImage
+        src={getPhotoUrl({
+          isThumbnail: true,
+          photoSrc: src
+        })}
+      />
+    </StyledCell>
+  )
+}
 
 const StyledImage = styled.img`
   border: 20px solid #bbb;
@@ -18,34 +66,19 @@ const StyledImage = styled.img`
   }
 `
 
-const StyledCell = styled.div<{
+const StyledCell = styled(motion.div)<{
   $borderColor: string
   $hoverBorderColor: string
+  $opacity: number
 }>`
   border: ${props => `4px solid ${props.$borderColor}`};
   margin: ${PHOTO_SPACING} 0;
-
+  opacity: ${props => `${props.$opacity}`};
   transition: border-color 0.2s ease-in-out;
 
   &:hover {
     border-color: ${props => `${props.$hoverBorderColor}`};
   }
 `
-
-const Cell = ({
-  vibrantcolors: { darkVibrant, lightVibrant },
-  src
-}: PhotoType) => {
-  return (
-    <StyledCell $hoverBorderColor={lightVibrant!} $borderColor={darkVibrant!}>
-      <StyledImage
-        src={getPhotoUrl({
-          isThumbnail: true,
-          photoSrc: src
-        })}
-      />
-    </StyledCell>
-  )
-}
 
 export default Cell
