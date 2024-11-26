e903bc0ee01a48eaff390ce24deaafa6237881be
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-26 16:22:16 -0600

    pushing for Austin

--- photo-gallery/ui/src/App.tsx
+++ photo-gallery/ui/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Context from './context'
-// import PhotoMasonry from './Masonry'
-import Resize2 from './Resize2'
+import PhotoMasonry from './Masonry'
+// import Resize2 from './Resize2'
 
 const App = () => {
-  // return <PhotoMasonry />
+  return <PhotoMasonry />
   // return <Resize />
-return <Resize2 />
+  // return <Resize2 />
 }
 
 const WrappedApp = () => {
