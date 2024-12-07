4b49f3951aaa18225d6afcb9d1a0f3276f9c2a7d
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-12-07 15:43:49 -0600

    new doc

--- theme-explorations/src/App.tsx
+++ theme-explorations/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import BaseHtmlElements from './BaseHtmlElements'
 import Config from './Config'
 import EngineeringDoc from './EngineeringDoc'
+import EngineeringDoc2 from './EngineeringDoc2'
 import Sidebar from './Sidebar'
 import Explorations from './explorations'
 import './fonts.css'
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<BaseHtmlElements />} />
         <Route path="/explorations" element={<Explorations />} />
         <Route path="/engineeringdoc" element={<EngineeringDoc />} />
+        <Route path="/engineeringdoc2" element={<EngineeringDoc2 />} />
       </Routes>
       <Sidebar sidebar={<Config />} />
     </>
