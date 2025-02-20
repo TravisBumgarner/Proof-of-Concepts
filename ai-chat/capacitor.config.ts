9b6a1d5711e81fa947f720d2e390a1434163faa5
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-02-20 15:03:37 -0600

    farewell

--- ai-chat/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ai-chat',
-  webDir: 'dist'
-};
-
-export default config;
